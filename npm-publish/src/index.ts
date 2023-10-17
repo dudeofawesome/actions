5645f10ac6553a86b7891e7a3129af32c6b42aa1
Louis Orleans <louis@orleans.io>
2023-10-17 02:16:10 -0700

    ♻️ extract utils from main file

--- npm-publish/src/index.ts
+++ npm-publish/src/index.ts
@@ -1,121 +1,19 @@
-import { dirname, join, resolve } from 'node:path';
-import { readFile } from 'node:fs/promises';
-import { getInput, info, setOutput } from '@actions/core';
-import { getOctokit, context } from '@actions/github';
-import type { GitHub } from '@actions/github/lib/utils';
+import { join } from 'node:path';
+import { debug, info, setOutput } from '@actions/core';
+import { getOctokit } from '@actions/github';
 import { exec } from '@actions/exec';
-import type { PackageJson } from 'type-fest';
 
-import { ParseStringToArray } from '../../utils/parse-string-to-array';
-import { CheckNodePackage } from '../../utils/check-node-package';
 import { filter } from '../../utils/async-filter';
 import { log_tap } from '../../utils/log-tap';
-
-async function get_workspace_paths(
-  package_dir_path: string,
-  included_workspaces: undefined | string[],
-  excluded_workspaces: undefined | string[],
-): Promise<string[]> {
-  function filterExcluded(ws: string): boolean {
-    return !(excluded_workspaces ?? []).includes(ws);
-  }
-
-  if (Array.isArray(included_workspaces) && included_workspaces.length > 0) {
-    // filter out excluded
-    return included_workspaces.filter(filterExcluded);
-  } else {
-    // read in workspaces from primary package.json
-    const package_json: PackageJson = await read_node_package(
-      join(package_dir_path, 'package.json'),
-    );
-    if (Array.isArray(package_json.workspaces)) {
-      return package_json.workspaces.filter(filterExcluded);
-    } else {
-      return [package_dir_path];
-    }
-  }
-}
-
-type PackageJsonWithPaths = PackageJson & {
-  workspace_path: string;
-  parent_package: string;
-};
-function read_node_package(
-  path: string,
-  workspace: string | undefined = undefined,
-): Promise<PackageJson | PackageJsonWithPaths> {
-  return readFile(path)
-    .then((buffer) => buffer.toString())
-    .then((string) => JSON.parse(string) as unknown)
-    .then((object) => CheckNodePackage(object))
-    .then(
-      (pkg) =>
-        ({
-          ...pkg,
-          ...(workspace != null
-            ? { workspace_path: dirname(path), parent_package: workspace }
-            : {}),
-        }) as PackageJson,
-    );
-}
-
-function get_inputs(): {
-  github_token: string;
-  package_dir_path: string;
-  included_workspaces: string[];
-  excluded_workspaces: string[];
-} {
-  const github_token = getInput('github-token');
-  const package_dir_path = resolve(getInput('package-dir-path'));
-  const included_workspaces = ParseStringToArray(
-    getInput('included-workspaces'),
-  ).map((p) => resolve(p));
-  const excluded_workspaces = ParseStringToArray(
-    getInput('excluded-workspaces'),
-  ).map((p) => resolve(p));
-
-  if (github_token.length === 0) {
-    throw new TypeError(`github-token must be set.`);
-  }
-
-  if (package_dir_path.endsWith('package.json')) {
-    throw new TypeError(
-      `\`package_dir_path\` should be a path to a directory (ie, should not end with "package.json")`,
-    );
-  }
-  for (const workspace of [...included_workspaces, ...excluded_workspaces]) {
-    if (workspace.endsWith('package.json')) {
-      throw new TypeError(
-        `workspaces should be a path to a directory (ie, should not end with "package.json")`,
-      );
-    }
-  }
-
-  return {
-    github_token,
-    package_dir_path,
-    included_workspaces,
-    excluded_workspaces,
-  };
-}
-
-function is_beta(pkg: PackageJson): boolean {
-  return pkg.version?.match(/^[\d.]+-.+$/u) != null;
-}
-
-async function is_default_branch(
-  octokit: InstanceType<typeof GitHub>,
-): Promise<boolean> {
-  const {
-    data: { default_branch },
-  } = await octokit.rest.repos.get({ ...context.repo });
-
-  return context.ref === default_branch;
-}
+import {
+  get_inputs,
+  get_workspace_paths,
+  is_beta,
+  is_default_branch,
+  read_node_package,
+} from './utils';
 
 async function main() {
-  info(`cwd: ${process.cwd()}`);
-
   const {
     github_token,
     package_dir_path,
@@ -125,7 +23,6 @@ async function main() {
   const octokit = getOctokit(github_token);
 
   process.chdir(package_dir_path);
-  info(`cwd: ${process.cwd()}`);
 
   const packages = await get_workspace_paths(
     package_dir_path,
@@ -143,10 +40,10 @@ async function main() {
         ),
       ),
     )
-    .then(log_tap(info, 'all package.json files'))
+    .then(log_tap(debug, 'all package.json files'))
     // filter out packages that aren't published to registry
     .then((packages) => filter(packages, (pkg) => pkg.private !== true))
-    .then(log_tap(info, 'publishable packages'))
+    .then(log_tap(debug, 'publishable packages'))
     // filter out packages whose version is already published
     .then((packages) =>
       filter(
@@ -161,7 +58,7 @@ async function main() {
           }),
       ),
     )
-    .then(log_tap(info, 'unpublished versions'))
+    .then(log_tap(debug, 'unpublished versions'))
     // are we on the default branch, or is it a prerelease version?
     .then((packages) =>
       filter(
@@ -169,7 +66,7 @@ async function main() {
         async (pkg) => (await is_default_branch(octokit)) || is_beta(pkg),
       ),
     )
-    .then(log_tap(info, 'default branch / prerelease versions'));
+    .then(log_tap(debug, 'default branch / prerelease versions'));
 
   // publish packages
   await Promise.all(
---
+++ npm-publish/src/utils.ts
@@ -0,0 +1,111 @@
+import { dirname, join, resolve } from 'node:path';
+import { readFile } from 'node:fs/promises';
+import { getInput } from '@actions/core';
+import { context } from '@actions/github';
+import type { GitHub } from '@actions/github/lib/utils';
+import type { PackageJson } from 'type-fest';
+
+import { ParseStringToArray } from '../../utils/parse-string-to-array';
+import { CheckNodePackage } from '../../utils/check-node-package';
+
+export async function get_workspace_paths(
+  package_dir_path: string,
+  included_workspaces: undefined | string[],
+  excluded_workspaces: undefined | string[],
+): Promise<string[]> {
+  function filterExcluded(ws: string): boolean {
+    return !(excluded_workspaces ?? []).includes(ws);
+  }
+
+  if (Array.isArray(included_workspaces) && included_workspaces.length > 0) {
+    // filter out excluded
+    return included_workspaces.filter(filterExcluded);
+  } else {
+    // read in workspaces from primary package.json
+    const package_json: PackageJson = await read_node_package(
+      join(package_dir_path, 'package.json'),
+    );
+    if (Array.isArray(package_json.workspaces)) {
+      return package_json.workspaces.filter(filterExcluded);
+    } else {
+      return [package_dir_path];
+    }
+  }
+}
+
+type PackageJsonWithPaths = PackageJson & {
+  workspace_path: string;
+  parent_package: string;
+};
+export function read_node_package(
+  path: string,
+  workspace: string | undefined = undefined,
+): Promise<PackageJson | PackageJsonWithPaths> {
+  return readFile(path)
+    .then((buffer) => buffer.toString())
+    .then((string) => JSON.parse(string) as unknown)
+    .then((object) => CheckNodePackage(object))
+    .then(
+      (pkg) =>
+        ({
+          ...pkg,
+          ...(workspace != null
+            ? { workspace_path: dirname(path), parent_package: workspace }
+            : {}),
+        }) as PackageJson,
+    );
+}
+
+export function get_inputs(): {
+  github_token: string;
+  package_dir_path: string;
+  included_workspaces: string[];
+  excluded_workspaces: string[];
+} {
+  const github_token = getInput('github-token');
+  const package_dir_path = resolve(getInput('package-dir-path'));
+  const included_workspaces = ParseStringToArray(
+    getInput('included-workspaces'),
+  ).map((p) => resolve(p));
+  const excluded_workspaces = ParseStringToArray(
+    getInput('excluded-workspaces'),
+  ).map((p) => resolve(p));
+
+  if (github_token.length === 0) {
+    throw new TypeError(`github-token must be set.`);
+  }
+
+  if (package_dir_path.endsWith('package.json')) {
+    throw new TypeError(
+      `\`package_dir_path\` should be a path to a directory (ie, should not end with "package.json")`,
+    );
+  }
+  for (const workspace of [...included_workspaces, ...excluded_workspaces]) {
+    if (workspace.endsWith('package.json')) {
+      throw new TypeError(
+        `workspaces should be a path to a directory (ie, should not end with "package.json")`,
+      );
+    }
+  }
+
+  return {
+    github_token,
+    package_dir_path,
+    included_workspaces,
+    excluded_workspaces,
+  };
+}
+
+export function is_beta(pkg: PackageJson): boolean {
+  return pkg.version?.match(/^[\d.]+-.+$/u) != null;
+}
+
+export async function is_default_branch(
+  octokit: InstanceType<typeof GitHub>,
+): Promise<boolean> {
+  const {
+    data: { default_branch },
+  } = await octokit.rest.repos.get({ ...context.repo });
+
+  return context.ref === default_branch;
+}
