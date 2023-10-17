6008d9c29355d68b128dc9805a65da15f6c310d3
Louis Orleans <louis@orleans.io>
2023-10-17 16:56:04 -0700

    🔨 improve tagging script

--- scripts/tag.rb
+++ scripts/tag.rb
@@ -12,6 +12,13 @@ end
 version=ARGV[0].start_with?('v') ? ARGV[0] : "v#{ARGV[0]}"
 semver=version.match(/^v(\d+)\.(\d+)\.(\d+)$/)
 
+# create new specific tag
 puts tag(version)
+
+# delete old major version tag
 puts `git tag --delete 'v#{semver[1]}'`
+puts `git push --delete origin 'v#{semver[1]}'`
+
+# create new major version tag
 puts tag("v#{semver[1]}")
+puts `git push origin 'v#{semver[1]}'`
