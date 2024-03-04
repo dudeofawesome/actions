9c438a846d1a4f9f2e80b05930096cccb054a8ef
Louis Orleans <louis@orleans.io>
2024-03-03 23:11:29 -0800

    📝 document testing

---
+++ npm-publish/CONTRIBUTING.md
@@ -0,0 +1,7 @@
+# Contributing
+
+1. Test your changes
+
+    ```bash
+    # GITHUB_TOKEN="$(gh auth token)" npm-publish/scripts/act.sh
+    ```
--- npm-publish/scripts/act.sh
+++ npm-publish/scripts/act.sh
@@ -1,17 +1,8 @@
 #!/usr/bin/env bash
-
-echo docker
-docker run \
-  --detach \
-  --rm \
-  --name verdaccio \
-  --publish '4873:4873' \
-  'docker.io/verdaccio/verdaccio:5'
+set -e
 
 echo build
 npm run build -w npm-publish/
 
 echo act
 act --secret GITHUB_TOKEN="$GITHUB_TOKEN" --workflows .github/workflows/npm-publish.yaml --job test
-
-docker stop verdaccio
